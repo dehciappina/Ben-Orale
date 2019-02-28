c030dd215844a99ea31d3e15f436ec90c080e8c3
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2019-02-28 15:17:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ const headerNav = document.querySelector('header nav')
 document.addEventListener("DOMContentLoaded", function() {
 
     setTimeout(() => {
-        logotypeOnly.style.transition = 'transform 2s cubic-bezier(0.4, 0, 0.2, 1), width 1.5s cubic-bezier(0.4, 0, 0.2, 1)'
+        logotypeOnly.style.transition = 'transform 1.5s cubic-bezier(0.4, 0, 0.2, 1), width 1.5s cubic-bezier(0.4, 0, 0.2, 1)'
 
         setTimeout(() => {
             whiteLoadingBG.style.opacity = 0;
             whiteLoadingBG.style.visibility = 'hidden';
 
-            logoOnly.style.transform = 'translateY(-13vh) scale(2.8)';
+            logoOnly.style.transform = 'translateY(-25vw) scale(2.8)';
             logoOnly.style.opacity = 0.2;
 
             logotypeOnly.style.transform = 'translateY(0vh)'
@@ -47,3 +47,22 @@ hamburger.addEventListener('click', showHiddenNav)
 document.querySelector('body').onscroll = function() {
     document.querySelector('.fixed_img_div img').style.transform = "translateY(" + window.scrollY + "px)"
 }
+
+
+
+
+
+
+// PWA
+
+if (navigator.serviceWorker.controller) {
+    console.log('[PWA Builder] active service worker found, no need to register')
+  } else {
+    //Register the ServiceWorker
+    navigator.serviceWorker.register('pwabuilder-sw.js', {
+      scope: './'
+    }).then(function(reg) {
+      console.log('Service worker has been registered for scope:'+ reg.scope);
+    });
+  }
+  
