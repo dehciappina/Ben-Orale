930b50edd21ce2a10c5b2a8b1933d784caef2fb6
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2019-03-03 11:46:20 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,9 +8,7 @@ const headerNav = document.querySelector('header nav')
 
 
 document.addEventListener("DOMContentLoaded", function() {
-
-  
-  if (window.matchMedia("(max-width: 180vh)").matches) {
+  if (window.matchMedia("(max-width: 100vh)").matches || window.matchMedia("(max-width: 170vh)")) {
     setTimeout(() => {
         logotypeOnly.style.transition = 'transform 1.5s cubic-bezier(0.4, 0, 0.2, 1), width 1.5s cubic-bezier(0.4, 0, 0.2, 1)'
 
@@ -22,15 +20,15 @@ document.addEventListener("DOMContentLoaded", function() {
             logoOnly.style.opacity = 0.2;
 
             logotypeOnly.classList.add('logotype_only_smaller')
-
-        }, 700);
-    }, 200);
+        }, 1000);
+    }, 400);
+  } else {
+    whiteLoadingBG.style.opacity = 0;
+    whiteLoadingBG.style.visibility = 'hidden';
+  
+    logotypeOnly.classList.add('logotype_only_smaller')
   }
   
-  whiteLoadingBG.style.opacity = 0;
-  whiteLoadingBG.style.visibility = 'hidden';
-
-  logotypeOnly.classList.add('logotype_only_smaller')
 
 });
 
@@ -66,7 +64,7 @@ let consultorioSectionHeight = document.querySelector('#consultorio').scrollHeig
 
 
 // MOBILE
-if (window.matchMedia("(max-width: 100vh)").matches) {
+if (window.matchMedia("(max-width: 100vh)").matches || window.matchMedia("(max-width: 170vh)").matches) {
   document.body.onscroll = function() {
       for(i = 0; i < fixedIMG.length; ++i) {
         fixedIMG[i].style.transform = "translateY(" + window.pageYOffset / 3 + "px)"
@@ -92,6 +90,7 @@ if (window.matchMedia("(max-width: 100vh)").matches) {
   localizacaoIMG.style.bottom = (consultorioSectionHeight + tratamentosSectionHeight) * 2 + 'px';
 
   document.querySelector('#tratamentos img').style.bottom = 75 + 'vw';
+
 }
 
 
