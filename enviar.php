8c37288c45408407481dd32cbadec33516ede484
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2019-03-06 15:33:46 -0300

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -1,17 +1,17 @@
 <?php
 
-$para = "ciappina_andre@hotmail.com";
+$para = "diego@playmidiapublicidade.com.br";
 $assunto = "Formulário de Contato";
 $mensagem = "<strong>Nome: </strong>".$_POST['nome'];
 $mensagem .= "<br><strong>E-mail: </strong>".$_POST['email'];
 $mensagem .= "<br><strong>Telefone: </strong>".$_POST['tel'];
 $mensagem .= "<br><strong>Mensagem: </strong>".$_POST['mensagem'];
 $cabecalho =  "Content-Type:text/html; charset=UTF-8\n";
-$cabecalho .= "From:  Contato pelo Site <ciappina_andre@hotmail.com>\n";
-$cabecalho .= "X-Sender:  <ciappina_andre@hotmail.com>\n";
+$cabecalho .= "From:  Contato pelo Site <diego@playmidiapublicidade.com.br>\n";
+$cabecalho .= "X-Sender:  <diego@playmidiapublicidade.com.br>\n";
 $cabecalho .= "X-Mailer: PHP  v".phpversion()."\n";
 $cabecalho .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
-$cabecalho .= "Return-Path:  <ciappina_andre@hotmail.com>\n";
+$cabecalho .= "Return-Path:  <diego@playmidiapublicidade.com.br>\n";
 $cabecalho .= "MIME-Version: 1.0\n";
 if(mail($para, $assunto, $mensagem, $cabecalho)) { 
      echo "<script type=\"text/javascript\">".
