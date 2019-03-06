986cd66ab2461645bdd46879a660edb9af3daea2
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2019-03-06 14:54:37 -0300

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -7,11 +7,11 @@ $mensagem .= "<br><strong>E-mail: </strong>".$_POST['email'];
 $mensagem .= "<br><strong>Telefone: </strong>".$_POST['tel'];
 $mensagem .= "<br><strong>Mensagem: </strong>".$_POST['mensagem'];
 $cabecalho =  "Content-Type:text/html; charset=UTF-8\n";
-$cabecalho .= "From:  Contato pelo Site <diego@playmidiapublicidade.com.br>\n";
-$cabecalho .= "X-Sender:  <diego@playmidiapublicidade.com.br>\n";
+$cabecalho .= "From:  Contato pelo Site <ciappina_andre@hotmail.com>\n";
+$cabecalho .= "X-Sender:  <ciappina_andre@hotmail.com>\n";
 $cabecalho .= "X-Mailer: PHP  v".phpversion()."\n";
 $cabecalho .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
-$cabecalho .= "Return-Path:  <diego@playmidiapublicidade.com.br>\n";
+$cabecalho .= "Return-Path:  <ciappina_andre@hotmail.com>\n";
 $cabecalho .= "MIME-Version: 1.0\n";
 if(mail($para, $assunto, $mensagem, $cabecalho)) { 
      echo "<script type=\"text/javascript\">".
