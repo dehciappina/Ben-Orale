15cd192e9a40cccf15c8b115f759720c3bddf946
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2019-04-10 13:18:49 -0300

    Add files via upload

--- tratamentos/script.js
+++ tratamentos/script.js
@@ -2,7 +2,6 @@
 const whiteLoadingBG = document.querySelector('.white_bg');
 const logoOnly = document.querySelector('.logo_only');
 const logotypeOnly = document.querySelector('.logotype_only');
-const boasVindas = document.querySelector('.boas_vindas');
 
 const hamburger = document.querySelector('.hamburger_container');
 const headerNav = document.querySelector('header nav')
@@ -34,18 +33,10 @@ if (!window.matchMedia("(min-width: 170vh)").matches) {
 
 document.addEventListener("DOMContentLoaded", function() {
   if (window.matchMedia("(min-width: 100vh)").matches || window.matchMedia("(min-width: 170vh)")) {
-        logotypeOnly.style.transition = 'none'
-
             whiteLoadingBG.style.opacity = 0;
             whiteLoadingBG.style.visibility = 'hidden';
 
-            logoOnly.style.transform = 'translateY(-25vw) scale(2)';
-
             logotypeOnly.classList.add('logotype_only_smaller');
-
-            setTimeout(() => {
-              boasVindas.style.opacity = 1;
-            }, 1600);
   } else {
     whiteLoadingBG.style.opacity = 0;
     whiteLoadingBG.style.visibility = 'hidden';
@@ -54,51 +45,31 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 });
 
-
 const fixedIMG = document.querySelectorAll('.fixed_img_div img')
-const tratamentosIMG = document.querySelector('#tratamentos div img')
-const consultorioSection = document.querySelector('#consultorio')
-const consultorioIMG = document.querySelector('#consultorio img')
-const localizacaoDiv = document.querySelector('#localizacao')
-const localizacaoIMG = document.querySelector('#localizacao img')
 
-const introPhoto = document.querySelector('.intro_subcontainer > div')
-const h1 = document.querySelector('h1')
+if (window.matchMedia("(min-width: 100vh)").matches || window.matchMedia("(min-width: 170vh)").matches) {
 
-let tratamentosSectionHeight = document.querySelector('#tratamentos').scrollHeight
-let consultorioSectionHeight = document.querySelector('#consultorio').scrollHeight
+    document.querySelector('#tratamentos img').style.bottom = 90 + 'vw';
 
-document.addEventListener('DOMContentLoaded', function() {
-  // DESKTOP
-  if (window.matchMedia("(min-width: 100vh)").matches || window.matchMedia("(min-width: 170vh)").matches) {
-  
-    document.querySelector('#tratamentos img').style.bottom = 62 + 'vw';
-    
-    consultorioIMG.style.bottom = tratamentosSectionHeight * 1.2 + 'px';
-  
     document.body.onscroll = function() {
         for(i = 0; i < fixedIMG.length; ++i) {
           fixedIMG[i].style.transform = "translateY(" + window.pageYOffset / 2 + "px)"
         }
-      h1.style.transform = "translateY(" + window.pageYOffset / 2.2 + "px)"
     }
+  
     // MOBILE
   } else {
-  
-    consultorioIMG.style.bottom = tratamentosSectionHeight / 2.5 + 'px';
-  
-    document.querySelector('#tratamentos img').style.bottom = 125 + 'vw';
-  
+
+    document.querySelector('#tratamentos img').style.bottom = 75 + 'vw';
+
     document.body.onscroll = function() {
       for(i = 0; i < fixedIMG.length; ++i) {
-        fixedIMG[i].style.transform = "translateY(" + window.pageYOffset / 3 + "px)";
+        fixedIMG[i].style.transform = "translateY(" + window.pageYOffset / 3 + "px)"
       }
     }
+    
   }
-})
-
-
-
+  
 
 // PWA
 
